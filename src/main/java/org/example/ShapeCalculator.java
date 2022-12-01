88ffbd4626dfd18b76cee0e23444b13daaa28864
Thibaut Cantet <thibaut.cantet@soat.fr>
2022-12-01 11:09:31 +0100

    With switch pattern matching

--- src/main/java/org/example/ShapeCalculator.java
+++ src/main/java/org/example/ShapeCalculator.java
@@ -3,9 +3,9 @@ package org.example;
 public class ShapeCalculator {
     public double getPerimeter(Shape shape) {
         return switch (shape) {
-            case Rectangle r: yield 2 * r.length() + 2 * r.width();
-            case Circle c:    yield 2 * c.radius() * Math.PI;
-            default:          throw new IllegalArgumentException("Unrecognized shape");
+            case Rectangle r -> 2 * r.length() + 2 * r.width();
+            case Circle c    -> 2 * c.radius() * Math.PI;
+            default          -> throw new IllegalArgumentException("Unrecognized shape");
         };
     }
 }
