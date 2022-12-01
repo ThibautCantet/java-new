b21913fe4918f4f9f630cd3eace2021ae4015070
Thibaut Cantet <thibaut.cantet@soat.fr>
2022-12-01 11:34:05 +0100

    With switch statement and yield

--- src/main/java/org/example/ShapeCalculator.java
+++ src/main/java/org/example/ShapeCalculator.java
@@ -2,12 +2,10 @@ package org.example;
 
 public class ShapeCalculator {
     public double getPerimeter(Shape shape) {
-        if (shape instanceof Rectangle r) {
-            return 2 * r.length() + 2 * r.width();
-        } else if (shape instanceof Circle c) {
-            return 2 * c.radius() * Math.PI;
-        } else {
-            throw new IllegalArgumentException("Unrecognized shape");
-        }
+        return switch (shape) {
+            case Rectangle r: yield 2 * r.length() + 2 * r.width();
+            case Circle c:    yield 2 * c.radius() * Math.PI;
+            default:          throw new IllegalArgumentException("Unrecognized shape");
+        };
     }
 }
