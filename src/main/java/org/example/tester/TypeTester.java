2b7528f60018e615aef8ec3cad462fbd249f929b
Thibaut Cantet <thibaut.cantet@soat.fr>
2022-12-01 15:25:41 +0100

    type tester : test short string with guarded pattern

--- src/main/java/org/example/tester/TypeTester.java
+++ src/main/java/org/example/tester/TypeTester.java
@@ -9,18 +9,12 @@ public class TypeTester {
     public String test(Object obj) {
         return switch (obj) {
             case null     -> "null";
-            case String s -> manageString(s);
+            case String s when s.length() < 10 -> "Short string: " + s;
+            case String s -> "Long string: " + s;
             case Color c  -> "Color: " + c.name();
             case Point p  -> p.toString();
             case int[] ia -> "Array: " + String.join(",", Arrays.stream(ia).mapToObj(String::valueOf).toList());
             default       -> "Something else";
         };
-    }
-
-    private static String manageString(String s) {
-        if (s.length() < 10) {
-            return "Short string: " + s;
-        }
-        return "Long string: " + s;
     }
 }
