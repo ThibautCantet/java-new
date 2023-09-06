6a4eb1a3de89a3da13d07849baf3c715a3d60305
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2023-09-06 14:43:31 +0200

    simplify main with java 21 with args

--- src/main/java/org/example/main/SimplifyMain.java
+++ src/main/java/org/example/main/SimplifyMain.java
@@ -1,6 +1,10 @@
 package org.example.main;
 
 public class SimplifyMain {
+    void main(String[] args) {
+        System.out.println("Main method with args");
+    }
+
     void main() {
         System.out.println("Hello, World!");
     }
