0716b5e195f01ed64c138973e09dce8c06c11c41
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2023-09-06 14:45:30 +0200

    simplify main with java 21 with static main and args

--- src/main/java/org/example/main/SimplifyMainStatic.java
+++ src/main/java/org/example/main/SimplifyMainStatic.java
@@ -1,6 +1,10 @@
 package org.example.main;
 
 public class SimplifyMainStatic {
+    static void main(String[] args) {
+        System.out.println("Static main method with args");
+    }
+
     static void main() {
         System.out.println("Hello, World!");
     }
